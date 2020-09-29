d2f0b3fab4b9438d2a23b474c55fd6f2ce3f79e9
Alexa-PL <platanaya48@gmail.com>
2020-09-29 23:39:55 +0300

    2. Реализовать функцию, принимающую несколько параметров, описывающих данные пользователя: имя, фамилия, год рождения, город проживания, email, телефон. Функция должна принимать параметры как именованные аргументы. Реализовать вывод данных о пользователе одной строкой.

---
+++ step_2.py
@@ -0,0 +1,5 @@
+def my_function(name, surname, year, city, email, phone):
+    print(name, surname, year, city, email, phone)
+
+
+my_function(name='Aleksandra', surname='Platanaya', year=1991, city='Korolev', email='email', phone='89262650255')
